8a66da71024a4672fa0a2cb8c0b43358b460b3aa
Mariam Ibrahim <ibrahimoyiza198@gmail.com>
2024-01-13 17:36:06 +0100

    write logic for todo app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './style.css'
+import './logic.js';
 
 console.log("New Project")
+// handle DOM manipulation
+// let newTodo = document.getElementById("new-todo");
+// newTodo.addEventListener('keydown',createTodo)
+// function createTodo(event){
+//     if(event.key=== 'Enter'|| event.code === 'Enter'){
+//     addTodo();
+//     newTodo.value="";
+//     }
+// }
+// console.log(TodoList);
